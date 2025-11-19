KoncertoGroup/fizzy

commit 6e304958be1c42974c55993c3359dc052603319d

No need to protect local dev/test

app/controllers/sessions_controller.rb
@@ -1,8 +1,12 @@
 class SessionsController < ApplicationController
   # FIXME: Remove this before launch!
-  SIGNUP_USERNAME = Rails.env.local? ? "testname" : Rails.application.credentials.account_signup_http_basic_auth.name
-  SIGNUP_PASSWORD = Rails.env.local? ? "testpassword" : Rails.application.credentials.account_signup_http_basic_auth.password
-  http_basic_authenticate_with name: SIGNUP_USERNAME, password: SIGNUP_PASSWORD, realm: "Fizzy Signup", only: :create, unless: -> { Identity.exists?(email_address: email_address) }
+  if Rails.env.remote?
+    http_basic_authenticate_with \
+      name: Rails.application.credentials.account_signup_http_basic_auth.name,
+      password: Rails.application.credentials.account_signup_http_basic_auth.password,
+      realm: "Fizzy Signup",
+      only: :create, unless: -> { Identity.exists?(email_address: email_address) }
+  end
 
   disallow_account_scope
   require_unauthenticated_access except: :destroy

commit 1f0fa500fa4e399946e5a23584afc8bae3984b6f

Restore basic auth on signup
broken in 6e304958

app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   # FIXME: Remove this before launch!
-  if Rails.env.remote?
+  unless Rails.env.local?
     http_basic_authenticate_with \
       name: Rails.application.credentials.account_signup_http_basic_auth.name,
       password: Rails.application.credentials.account_signup_http_basic_auth.password,